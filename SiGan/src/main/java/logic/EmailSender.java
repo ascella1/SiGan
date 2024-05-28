205d194fd7f93e173fe37b687aa6269b9d9e1931
ascella1 <chlwogns0108@gmail.com>
2024-05-28 13:48:25 +0900

    5/28 Main.java에 현재 시간 비교하고 이메일 보내는 로직 구현 완료, DataBaseInfo 모델 클래스 생성

--- SiGan/src/main/java/logic/EmailSender.java
+++ SiGan/src/main/java/logic/EmailSender.java
@@ -17,27 +17,45 @@ public class EmailSender {
         final String password = "ioqj hark yqsn opbd";
 
         // SMTP 서버 속성 설정
+        //Properties -> Hashtable을 상속받는 설정파일, 시스템 속성을 사용하는 클래스
+        
+        //현재 JVM의 속성을 가져온 후 properties 객체에 저장
         Properties properties = System.getProperties();
+        
+        //SMTP서버의 호스트 이름 설정
         properties.put("mail.smtp.host", host);
+        //SMTP서버의 포트번호 설정
         properties.put("mail.smtp.port", "587");
+        //서버 인증을 사용할것인지, true 라면 이메일을 보내기 위해 사용자 이름, 비밀번호 요구
         properties.put("mail.smtp.auth", "true");
+        //STARTTLS 명령을 통해 SMTP 연결을 암호화
         properties.put("mail.smtp.starttls.enable", "true");
 
+        
         // 인증을 포함한 세션 생성
+        //JavaMail API 를 사용하여 인증을 포함한 세션을 생성하는 방법
+        //주어진 속성과 인증정보를 사용하여 새로운 이메일 세션 생성
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+        	//Authenticator 클래스에서 인증정보를 제공하기 위해 오버라이드된 메소드
             protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+            	//javax.mail.PasswordAuthentication 객체 생성 후 사용자이름과 패스워드 반환
                 return new javax.mail.PasswordAuthentication(username, password);
             }
         });
 
+
         try {
             // 이메일 메시지 작성
+        	//MimeMessage 객체 생성 -> 이메일 메시지 표현
             MimeMessage message = new MimeMessage(session);
+            //이메일 발신자 설정 InternetAddress 클래스 -> 이메일 주소 나타내기 위함
             message.setFrom(new InternetAddress(from));
+            //수신자 주소 추가, Message.RecipientType.TO -> 수신자의 유형을 나타냄, TO는 주 수신자
+            //addRecipient 메소드는 하나이상의 수신자를 추가 
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
             message.setSubject(subject);
             message.setText(text);
-
+            
             // 이메일 전송
             Transport.send(message);
             System.out.println("메일 전송이 완료되었습니다.");
--- SiGan/src/main/java/main/Main.java
+++ SiGan/src/main/java/main/Main.java
@@ -1,16 +1,44 @@
 package main;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import logic.EmailSender;
 
 public class Main {
 	public static void main(String[] args) {
         // 테스트를 위한 이메일 전송
         String recipient = "chlwogns0108@gmail.com";
-        String subject = "Test Subject";
-        String text = "Hello, this is a test email!";
+        String subject = "시간 일치되어서 메일 전송";
+        String text = "시간 테스트 이메일 ";
         
-        EmailSender es = new EmailSender();
+        //테스트 시간
+        String targetTime = "2024-05-28 12:21:30";
         
-        es.sendEmail(recipient, subject, text);
+        //EmailSender es = new EmailSender(); -> static 으로 했으므로 객체 생성 필요 없음
+        //EmailSender.sendEmail(recipient, subject, text);
+        
+        
+        //계속 현재 시간을 확인해야 하므로 while(1)
+        while(true) {
+        	//현재시간을 가져오는 메소드
+        	String currentTime =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").
+        			format(new Date());
+        	//현재시간의 문자열과 테스트시간의 문자열이 일치하는지 확인
+        	System.out.println(currentTime);
+        	if(currentTime.equals(targetTime)) {
+        		System.out.println("시간 일치");
+        		//시간 일치하므로 메일 전송
+        		EmailSender.sendEmail(recipient, subject, text);
+        	}
+        	try {
+        		//CPU 사용률을 낮추기 위한 .sleep
+        		Thread.sleep(1000);
+        	}catch(InterruptedException e){
+        		e.printStackTrace();
+        	}
+        }
+        
+        
     }
 }
